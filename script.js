f82f92167dbc505dcedfa04c59d50639c007f768
capa7 <87338294+capa7@users.noreply.github.com>
2025-05-12 13:34:22 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// Inicializa AOS (animaciones)
+AOS.init({
+  duration: 1000,
+  easing: 'ease-in-out',
+  once: true,
+});
+
+// Configura partículas
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 40 },
+    size: { value: 3 },
+    color: { value: "#b47b84" },
+    line_linked: {
+      enable: true,
+      distance: 120,
+      color: "#b47b84",
+      opacity: 0.5,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 1
+    }
+  },
+  interactivity: {
+    events: {
+      onhover: { enable: true, mode: "repulse" }
+    }
+  }
+});
